2792f061877c63fda7adf5117a96e498e426cd4a
josh <102491975+joshingcode@users.noreply.github.com>
2022-08-25 12:39:33 -0400

    initial add

---
+++ main.js
@@ -0,0 +1,28 @@
+// use or fetch to get our picture of the day 
+
+
+document.querySelector('button').addEventListener('click',getFetch); 
+
+
+function getFetch(){ 
+let date= document.querySelector('input').value;
+const url = `https://api.nasa.gov/planetary/apod?api_key=DTwzGxGJhoXp5t8jIcONrSNZlksqvw0POU9U9KxM&date=${date}`;
+
+fetch(url)
+
+    .then(res => res.json()) // parse response as JSON
+
+    .then(data => {
+        document.querySelector('img').src= data.hdurl 
+        document.querySelector('h2').innerHTML=data.title
+        document.querySelector('p').innerHTML=data.explanation
+      console.log(data)
+
+    })
+
+    .catch(err => {
+
+        console.log(`error ${err}`)
+
+    });
+}
